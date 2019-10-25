8ccb451ba938f7fee6fc70cf14b4181ca322cbb4
Bright Ahiadeke <kweku01bright@gmail.com>
2019-10-25 04:40:11 +0000

    Commit build

--- build/63.js
+++ build/63.js
@@ -1,6 +1,6 @@
 webpackJsonp([63],{
 
-/***/ 2026:
+/***/ 2030:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- build/84.js
+++ build/84.js
@@ -1,6 +1,6 @@
 webpackJsonp([84],{
 
-/***/ 2090:
+/***/ 2093:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
